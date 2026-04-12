a817f8923d8e3edff0bb8783b7b5656b0a7548d7
zdenek.sotolar <sotolar.zdenek@gmail.com>
2026-04-12 09:57:41 +0200

    Revert "Bug no. 21 - Při registrace je pole pro heslo textové, heslo lze vidět"
    This reverts commit 27dd4f22b1bca80e64c008a49fb4775045fa2a84.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,7 +48,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">@lang('auth.registration.password')</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="text" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="off">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -62,7 +62,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">@lang('auth.registration.password_confirm')</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="off">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
